26b9f7f42ab52957ee623dee3355c3e5f32e3952
GitEBF <132492464+GitEBF@users.noreply.github.com>
2024-04-24 15:16:14 -0400

    Profil now works with ids
    Curently it will display an error because the user isnt connected

--- src/pages/Profil.tsx
+++ src/pages/Profil.tsx
@@ -6,17 +6,22 @@ import useFetch from '../hooks/useFetch';
 
 export default function() {
     const { id } = useParams()
-    const { loading, error, data } = useFetch('http://52.242.29.209:1337/api/compte-tuteurs/' + id)
+    const { loading, error, data } = useFetch('http://52.242.29.209:1337/api/users/' + id + "?populate=*")
 
     if (loading) return <p>Loading...</p>
-    if (error) return <p>Error :(</p>
+    if (error) return <p>Error</p>
+    if (!data) return <p>No data</p>
     console.log(data)
     let userColor = "default"
-    let userImage = "https://i.pravatar.cc/150?u=a042581f4e29026024d" //Juste "" enleve l'image
-    let userName = "John Doe Ding Dong Ditch"
-    let userEmail = "john.doe.01@edu.cegeptr.qc.ca"
-    let userApp = "4.3☆"
+    let userImage = "http://52.242.29.209:1337" + data.picture.url
+    let userName = data.firstName + " " + data.lastName
+    let userEmail = data.email
+    let userApp = data.reviewAvg + "☆"
     let userType = "Étudiant"
+    if (data.isTeacher) {
+        userType = "Enseignant"
+    }
+    
     
     return (
         <>
