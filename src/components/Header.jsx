7b5102ecc33ea64331b0aec450efa631c2a8f85b
Nullify <132492464+GitEBF@users.noreply.github.com>
2024-04-28 19:19:26 -0400

    Read Desc
    Changed from tsx to jsx
    Functional login and bearing token
    Added two hooks

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+const Header = () => {
+  return (
+    <div className="text-center fixed top-0 z-50 w-full h-[4vh]"></div>
+  )
+}
+/*{import.meta.env.VITE_API_URL}*/
+export default Header
--- src/components/Header.tsx
+++
@@ -1,7 +0,0 @@
-const Header = () => {
-  return (
-    <div className="text-center fixed top-0 z-50 w-full h-[4vh]">{import.meta.env.VITE_API_URL}</div>
-  )
-}
-
-export default Header
